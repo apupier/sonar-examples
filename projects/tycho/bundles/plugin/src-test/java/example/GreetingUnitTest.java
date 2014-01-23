ed467912ee536707827cb090b4d1c516c1c7dc9d
Aurelien Pupier <aurelien.pupier@bonitasoft.com>
2014-01-23 16:17:37 +0100

    Working code coverage of Unit and integration tests with only a limited hierarchy depth

---
+++ projects/tycho/bundles/plugin/src-test/java/example/GreetingUnitTest.java
@@ -0,0 +1,12 @@
+package example;
+
+import org.junit.Test;
+
+public class GreetingUnitTest
+{
+	@Test
+	public void coveredByUnitTest()
+    {
+       new Greeting().coveredByUnitTest();
+    }
+}
---
+++ projects/tycho/bundles/plugin/src/example/Greeting.java
@@ -0,0 +1,14 @@
+package example;
+
+public class Greeting
+{
+    public void coveredByTest()
+    {
+       System.out.println("Hello, world.");
+    }
+	
+	    public void coveredByUnitTest()
+    {
+       System.out.println("Hello, world.");
+    }
+}
